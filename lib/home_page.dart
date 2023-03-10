feaee5074b3b60d96533430c1fffdafab4b89644
SheikhAman <amansk664@gmail.com>
2023-03-10 13:05:11 +0600

    getx : snackbar, default dialog, dialog

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_series/controller.dart';
-import 'package:get_series/total_page.dart';
+
+import 'controller.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -12,104 +12,44 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Text(
-                  "Books",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    c.incrementBooks();
-                  },
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Obx(
-                  () => Text(
-                    c.books.value.toString(),
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    c.decrementBooks();
-                  },
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Pens",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    c.incrementPens();
-                  },
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Obx(
-                  () => Text(
-                    c.pen.value.toString(),
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    c.decrementPens();
-                  },
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            GetBuilder<Controller>(
-              builder: (controller) => Text(
-                controller.bag.toString(),
-                style: TextStyle(fontSize: 20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // SnackBar
+              ElevatedButton(
+                onPressed: () {
+                  Get.snackbar("hello", "this is snackbar");
+                },
+                child: Text("SnackBar"),
               ),
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                c.incrementBag();
-              },
-              child: Text("increament"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => TotalPage());
-              },
-              child: const Text("Total"),
-            ),
-          ],
+              // default dialog
+              ElevatedButton(
+                onPressed: () {
+                  Get.defaultDialog(title: "I am a dialog");
+                },
+                child: Text("default Dialog"),
+              ),
+              // dialog
+              ElevatedButton(
+                onPressed: () {
+                  Get.dialog(
+                    AlertDialog(
+                      title: const Text('Dialog'),
+                      content: const Text('This is a dialog'),
+                      actions: [
+                        TextButton(
+                          child: const Text("Close"),
+                          onPressed: () => Get.back(),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: Text("Dialog"),
+              ),
+            ],
+          ),
         ),
       ),
     );
